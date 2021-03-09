8eff9cb31149ffbaa13b90bdb23eebbd81df66b9
namtran-code <tnamqb@gmail.com>
2021-03-09 16:28:53 +0700

    add btkiemtranamnhuan forder

---
+++ ktnamnhuan.js
@@ -0,0 +1,18 @@
+let year = parseInt(prompt("Enter a year"));
+let isLeapYear = false;
+
+if (year % 4 == 0) {
+  if (year % 100 == 0) {
+    if (year % 400 == 0) {
+      isLeapYear = true;
+    }
+  } else {
+    isLeapYear = true;
+  }
+}
+
+if (isLeapYear) {
+  alert(year + " is a leap year");
+} else {
+  alert(year + " is NOT a leap year");
+}
